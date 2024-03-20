b5de9a85410950f16bb294d8c0325b89deb2b681
Arthur <wuthrich.famille@gmail.com>
2024-03-20 11:57:52 +0100

    Fin du cours

--- DB/liste_ajout.php
+++ DB/liste_ajout.php
@@ -3,7 +3,8 @@
 include('/DB_connexion.php');
 
 // Requête pour récupérer les lieux
-$sqlLieu = "SELECT * FROM t_lieu";
+$sqlLieu = "SELECT * FROM t_lieu 
+WHERE id_lieu in(1,2)";
 $resultLieu = $conn->query($sqlLieu);
 
 // Requête pour récupérer les types de manifestation

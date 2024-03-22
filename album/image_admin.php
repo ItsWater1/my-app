9923afa9ec54f7d85066203ee800f8c5bec7236c
Arthur <wuthrich.famille@gmail.com>
2024-03-22 12:27:00 +0100

    k

---
+++ album/image_admin.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: /my-app/login.php");
+    exit();
+}
+
+include('../DB/DB_connexion.php');
+include('../ressources/nav_adm.php');
+include('../ressources/footer.php');
+include('imageModel.php');
+
+$imageModel = new ImageModel($conn);
+$images = $imageModel->getAllImages();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration des images</title>
+    <link rel="stylesheet" href="/my-app/ressources/album.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <h2 class="text-center mb-4">Administration des images</h2>
+    <div class="row justify-content-center">
+        <?php foreach ($images as $image):
+            // Définir le fuseau horaire
+            date_default_timezone_set('Europe/Zurich');?>
+            <div class="col-md-3">
+                <div class="image-container">
+                    <img src="uploads/<?= $image['filename'] ?>" alt="Photo" class="img-thumbnail">
+                    <div class="image-details">
+                        <p><strong>Date :</strong> <?= date('d.m.Y', strtotime($image['date'])) ?></p>
+                        <p><strong>Lieu :</strong> <?= $image['NomLieu'] ?></p>
+                        <a href="image_delete.php?id=<?= $image['id_image'] ?>" class="btn btn-danger">Supprimer</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+</body>
+</html>
---
+++ album/image_delete.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: /my-app/login.php");
+    exit();
+}
+
+include('../DB/DB_connexion.php');
+include('imageModel.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
+    $image_id = $_GET['id'];
+    $imageModel = new ImageModel($conn);
+    $success = $imageModel->deleteImage($image_id);
+
+    if ($success) {
+        // Suppression réussie dans la base de données, maintenant supprimer le fichier
+        $filename = $imageModel->getImageFilename($image_id);
+        $file_path = "/my-app/album/uploads/{$filename}"; // Assurez-vous que le chemin est correct
+
+        // Vérifier si le fichier existe avant de tenter de le supprimer
+        if (file_exists($file_path)) {
+            unlink($file_path); // Supprime le fichier image du dossier
+        }
+    }
+
+    header("Location: /my-app/album/image_admin.php");
+    exit();
+}
+?>
